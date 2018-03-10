2230e9eda2a9ceb9f16f7f3db7ca12f24e3304b2
James <J.Lemons2@gmail.com>
2018-03-10 00:06:21 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (type) VALUES ("bacon chesseburger");
+INSERT INTO burgers (type, ordered) VALUES ('bacon patty with grilled cheese', true);
+INSERT INTO burgers (type) VALUES ("triple patty bacon with the fixings");
